889b614e9ee8a2fa06e6a8602cb43ba941c66773
EmanueleCervone <emanuelecervone@itsincom.it>
2025-03-20 15:48:29 +0100

    Sistemato la visualizzazione delle visite e dei dipendenti nella portineria

--- src/main/java/it/itsincom/webdevd/ReceptionResource.java
+++ src/main/java/it/itsincom/webdevd/ReceptionResource.java
@@ -2,18 +2,21 @@ package it.itsincom.webdevd;
 
 import io.quarkus.qute.Template;
 import io.quarkus.qute.TemplateInstance;
+import it.itsincom.webdevd.model.User;
 import it.itsincom.webdevd.model.Visit;
 import it.itsincom.webdevd.service.CookiesSessionManager;
+import it.itsincom.webdevd.service.UsersManager;
 import it.itsincom.webdevd.service.VisitsManager;
 import it.itsincom.webdevd.web.validation.CredentialValidationErrors;
 import it.itsincom.webdevd.web.validation.UserValidator;
-import jakarta.ws.rs.CookieParam;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.NewCookie;
 import jakarta.ws.rs.core.Response;
 
 import java.io.*;
+import java.net.URI;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 
 @Path("/reception")
@@ -22,12 +25,14 @@ public class ReceptionResource {
     private final VisitsManager visitsManager;
     private final Template login;
     private final CookiesSessionManager cookiesSessionManager;
+    private final UsersManager usersManager;
 
-    public ReceptionResource(Template reception, VisitsManager visitsManager, Template login, CookiesSessionManager cookiesSessionManager) {
+    public ReceptionResource(Template reception, VisitsManager visitsManager, Template login, CookiesSessionManager cookiesSessionManager, UsersManager usersManager) {
         this.reception = reception;
         this.visitsManager = visitsManager;
         this.login = login;
         this.cookiesSessionManager = cookiesSessionManager;
+        this.usersManager = usersManager;
     }
 
 
@@ -44,9 +49,59 @@ public class ReceptionResource {
                     .build();
         }
 
-        System.out.println("Accessing Reception page");
+        String fiscalCode = cookiesSessionManager.getUserFromSession(sessionId).getFiscalCode();
+        String nameUser = cookiesSessionManager.getUserFromSession(sessionId).getNameSurname();
+        System.out.println(fiscalCode);
+        List<Visit> visits = VisitsManager.getAllVisits();
+        LocalDate today = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        today.format(formatter);
+        List<User> users = UsersManager.getAllEmployees();
 
-        return Response.ok(reception.instance().data("visit", visitsAll)).build();
+        if (visits == null || visits.isEmpty()) {
+            return Response.ok(reception.instance()).build();
+        }
+
+        visits.sort(Comparator.comparing(Visit::getDate).thenComparing(Visit::getStartTime).reversed());
+        return Response.ok(reception.data("visit", visits , "today", today, "nome",nameUser , "employees", users)).build();
+    }
+
+    @GET
+    @Path("/sort")
+    public Response sortVisit(@QueryParam("dateSort") String dateSort, @CookieParam(CookiesSessionManager.COOKIE_SESSION) String sessionId) {
+        LocalDate datePr;
+        List<Visit> visits;
+        visits = VisitsManager.getAllVisits();
+        if (visits == null || visits.isEmpty()) {
+            return Response.status(Response.Status.NOT_FOUND).entity("No visits found").build();
+        }
+        LocalDate today = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        today.format(formatter);
+        String nameUser = cookiesSessionManager.getUserFromSession(sessionId).getNameSurname();
+        if (dateSort == null){
+            visits.sort(Comparator.comparing(Visit::getDate).thenComparing(Visit::getStartTime).reversed());
+            visits.removeLast();
+            System.out.println(visits);
+            return Response.ok(reception.data("visit", visits , "today", today, "nome",nameUser)).build();
+        }
+        datePr = LocalDate.parse(dateSort);
+
+
+        List <Visit> visitsWithSpecificDate = null;
+        visitsWithSpecificDate = visits.stream().filter(v-> v.getLocalDate().isEqual(datePr)).toList();
+        return Response.ok(reception.data("visit", visitsWithSpecificDate , "today", today, "nome",nameUser)).build();
+    }
+
+    @POST
+    @Path("/logout")
+    public Response logout(@CookieParam(CookiesSessionManager.COOKIE_SESSION) String SessionId) {
+        if (SessionId != null) {
+            cookiesSessionManager.removeUserFromSession(SessionId);
+        }
+        return Response.seeOther(URI.create("/login"))
+                .cookie(new NewCookie(cookiesSessionManager.COOKIE_SESSION, "", "/login", null, "Session expired", 0, false))
+                .build();
     }
 
 }
